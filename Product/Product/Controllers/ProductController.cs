dbacb8aa1083b5e589f1abb33c47a6c7dc60c343
ken wang <284843225@qq.com>
2018-10-19 16:24:21 +0800

    Init code for identityserver4 demo code

---
+++ Product/Product/Controllers/ProductController.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Product;
+
+namespace Api.Controllers
+{
+    [Route("[controller]")]
+    [Authorize]
+    public class ProductController : ControllerBase
+    {
+        // GET api/product
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var userIdentitys = from c in User.Claims
+                                select new UserIdentity
+                                {
+                                    Type = c.Type,
+                                    Value = c.Value
+                                };
+            var result = new UserIdentityModel()
+            {
+                Description = "Access user product api successfully",
+                UserIdentitys = userIdentitys.ToList()
+            };
+
+            return new JsonResult(result);
+        }
+    }
+}
